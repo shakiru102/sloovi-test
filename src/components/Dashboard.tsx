ea155783f108abf3f039ac6b32e01d024af8958e
shakiru102 <ashakiru53@gmail.com>
2022-08-26 14:19:22 +0100

    fixes

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -29,7 +29,7 @@ const Dashboard = () => {
    }
   }, [ user ])
    
-  if(!userList) return <div>Loading....</div>
+  if(!user || !userList) return <div>Loading....</div>
 
   return (
     <div className='h-[100vh] flex'>
